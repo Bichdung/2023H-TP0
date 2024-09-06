64b13b4ddbf5d61e1d8d055aee0dafdb90d31914
William Nguyen Le <17170192@reginaassumpta.qc.ca>
2024-09-06 10:03:11 -0400

    Réponse à la question 1 complétée

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
-print(math.pi * rayon^2)
+print(math.pi * rayon**2)
 
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
 
